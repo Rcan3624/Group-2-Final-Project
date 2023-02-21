81d4ef8f8cc7904bc0274efd41768e6ac60e6b09
Ryan Cannon <55251953+Rcan3624@users.noreply.github.com>
2023-02-21 22:44:20 +0000

    Restructured the file system temporarily so that everything will work without any problems.

--- readme.txt
+++
@@ -1,1 +0,0 @@
-This is where the web pages will be placed after they are functioning properly
--- test.js
+++
@@ -1,2 +0,0 @@
-// Testing out the javascript for the faq section. Still need to get it working properly
-document.getElementById("answer1").innerHTML = "Test"
